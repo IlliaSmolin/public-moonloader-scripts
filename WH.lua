a7712e868d5f9b84f579a05ab5d127f0b173a89f
IlliaSmolin <smolinilusha@gmail.com>
2020-12-04 22:34:56 +0200

    updated WH to v1.5, added different colors for weapon distances

--- WH.lua
+++ WH.lua
@@ -1,11 +1,14 @@
 script_name("WH")
 script_description("Wall hack with info about enemies' health, shots to die from deagle, and with anti-screenshot mode")
-script_version("v1.4")
+script_version("v1.5")
 script_authors("YoungDaggerD")
 
 require 'lib.moonloader'
 local font = renderCreateFont('Arial',8,5)
 local active = false
+local globalDistance = 100.0
+local deagleDistance = 35.0
+local m4Distance = 90.0
 
 function activation()
 	active = not active
@@ -38,15 +41,21 @@ function main()
 
 			for k,v in ipairs(charsTable) do 
 				local posX, posY, posZ = getCharCoordinates(v)
-				if getDistanceBetweenCoords3d(posX, posY, posZ, pPosX, pPosY, pPosZ) <= 100.0 then
+				if getDistanceBetweenCoords3d(posX, posY, posZ, pPosX, pPosY, pPosZ) <= globalDistance then
 					local wPosX, wPosY = convert3DCoordsToScreen(posX, posY, posZ)
 					res, id = sampGetPlayerIdByCharHandle(v)
 					hp = sampGetPlayerHealth(id)
 					armor = sampGetPlayerArmor(id)
 					globalHealth = hp + armor
 					if res then
-						renderDrawBoxWithBorder(wPosX - 35, wPosY - 40, 65, 95, 0x00FFFFFF, 1, 0xFF00FF00)
-						renderFontDrawText(font, '{FF0000}'..math.ceil((globalHealth)/47), wPosX + 10, wPosY + 20, -1)
+						if getDistanceBetweenCoords3d(posX, posY, posZ, pPosX, pPosY, pPosZ) >= deagleDistance and getDistanceBetweenCoords3d(posX, posY, posZ, pPosX, pPosY, pPosZ) <= m4Distance then
+							renderDrawBoxWithBorder(wPosX - 35, wPosY - 40, 65, 95, 0x00FFFFFF, 1, 0xFFFFFF00)
+						elseif getDistanceBetweenCoords3d(posX, posY, posZ, pPosX, pPosY, pPosZ) <= deagleDistance then
+							renderDrawBoxWithBorder(wPosX - 35, wPosY - 40, 65, 95, 0x00FFFFFF, 1, 0xFFF00F00)
+						else
+							renderDrawBoxWithBorder(wPosX - 35, wPosY - 40, 65, 95, 0x00FFFFFF, 1, 0xFFFFFFFF)
+						end
+						renderFontDrawText(font, '{FFF000}'..math.ceil((globalHealth)/47), wPosX + 10, wPosY + 20, -1)
 						renderFontDrawText(font, globalHealth, wPosX, wPosY + 35, -1)
 					end
 				end
